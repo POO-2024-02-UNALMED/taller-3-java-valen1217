5551b1e10958bdd614f64b6daf7fc7170ba67e79
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-19 16:27:50 -0400

    Update control.java

--- src/main/java/taller3/televisores/control.java
+++ src/main/java/taller3/televisores/control.java
@@ -1,129 +1,64 @@
-class Control:
-    def _init_(self):
-        self.__tv = None
+public class Control {
+    private TV tv; 
 
-    def enlazar(self, televisor):
-        self.__tv = televisor
-        televisor.setControl(self)
+    public TV getTv() { 
+        return tv;
+    }
 
-    def getTv(self):
-        return self.__tv
+    public void setTv(TV tv) { 
+        this.tv = tv;
+    }
 
-    def setTv(self, televisor):
-        self.__tv = televisor
+    public void enlazar(TV tv) { 
+        this.tv = tv;
+        tv.setControl(this);
+    }
 
-    def turnOn(self):
-        if self.__tv:
-            self.__tv.turnOn()
+    public void turnOn() {
+        if (tv != null) {
+            tv.turnOn();
+        }
+    }
 
-    def turnOff(self):
-        if self.__tv:
-            self.__tv.turnOff()
+    public void turnOff() {
+        if (tv != null) {
+            tv.turnOff();
+        }
+    }
 
-    def canalUp(self):
-        if self.__tv:
-            self.__tv.canalUp()
+    public void canalUp() {
+        if (tv != null) {
+            tv.canalUp();
+        }
+    }
 
-    def canalDown(self):
-        if self.__tv:
-            self.__tv.canalDown()
+    public void canalDown() {
+        if (tv != null) {
+            tv.canalDown();
+        }
+    }
 
-    def volumenUp(self):
-        if self.__tv:
-            self.__tv.volumenUp()
+    public void volumenUp() {
+        if (tv != null) {
+            tv.volumenUp();
+        }
+    }
 
-    def volumenDown(self):
-        if self.__tv:
-            self.__tv.volumenDown()
+    public void volumenDown() {
+        if (tv != null) {
+            tv.volumenDown();
+        }
+    }
 
-    def setCanal(self, canal):
-        if self.__tv:
-            self.__tv.setCanal(canal)
+    public void setCanal(int canal) {
+        if (tv != null) {
+            tv.setCanal(canal);
+        }
+    }
 
-class Marca:
-    def _init_(self, nombre):
-        self.__nombre = nombre
-
-    def getNombre(self):
-        return self.__nombre
-
-    def setNombre(self, nuevoNombre):
-        self.__nombre = nuevoNombre
-
-
-class TV:
-    numTV = 0
-
-    def _init_(self, marca, encendido):
-        self.__marca = marca
-        self.__canal = 1
-        self.__precio = 500
-        self.__volumen = 1
-        self.__estado = encendido
-        self.__control = None
-        TV.numTV += 1
-
-    def getMarca(self):
-        return self.__marca
-
-    def setMarca(self, marcaNueva):
-        self.__marca = marcaNueva
-
-    def getCanal(self):
-        return self.__canal
-
-    def setCanal(self, nuevoCanal):
-        if self.__estado and 1 <= nuevoCanal <= 120:
-            self.__canal = nuevoCanal
-
-    def getPrecio(self):
-        return self.__precio
-
-    def setPrecio(self, precioNuevo):
-        self.__precio = precioNuevo
-
-    def getVolumen(self):
-        return self.__volumen
-
-    def setVolumen(self, nuevoVolumen):
-        if self.__estado and 0 <= nuevoVolumen <= 7:
-            self.__volumen = nuevoVolumen
-
-    def getControl(self):
-        return self.__control
-
-    def setControl(self, controlNuevo):
-        self.__control = controlNuevo
-
-    @staticmethod
-    def getNumTV():
-        return TV.numTV
-
-    @staticmethod
-    def setNumTV(cantidad):
-        TV.numTV = cantidad
-
-    def turnOn(self):
-        self.__estado = True
-
-    def turnOff(self):
-        self.__estado = False
-
-    def getEstado(self):
-        return self.__estado
-
-    def canalUp(self):
-        if self._estado and self._canal < 120:
-            self.__canal += 1
-
-    def canalDown(self):
-        if self._estado and self._canal > 1:
-            self.__canal -= 1
-
-    def volumenUp(self):
-        if self._estado and self._volumen < 7:
-            self.__volumen += 1
-
-    def volumenDown(self):
-        if self._estado and self._volumen > 0:
-            self.__volumen -= 1
+    public void setVolumen(int volumen) {
+        if (tv != null) {
+            tv.setVolumen(volumen);
+        }
+    }
+}
