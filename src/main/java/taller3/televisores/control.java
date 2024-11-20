af990650861cace49e6ad82b86c71c63be2962df
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-20 05:28:19 -0400

    Update control.java

--- src/main/java/taller3/televisores/control.java
+++ src/main/java/taller3/televisores/control.java
@@ -1,17 +1,17 @@
 public class Control {
-    private TV tv; 
+    private TV tv;
 
-    public TV getTv() { 
+    public void enlazar(TV tv) {
+        this.tv = tv;
+        tv.setControl(this);
+    }
+
+    public TV getTv() {
         return tv;
     }
 
-    public void setTv(TV tv) { 
+    public void setTv(TV tv) {
         this.tv = tv;
-    }
-
-    public void enlazar(TV tv) { 
-        this.tv = tv;
-        tv.setControl(this);
     }
 
     public void turnOn() {
